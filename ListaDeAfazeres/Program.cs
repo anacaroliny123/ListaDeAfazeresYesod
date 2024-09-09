5b02f836e4ac4ce7c0a1399ffab917596291f1aa
carol <anacarolinymachadosilva@gmail.com>
2024-09-09 17:03:44 -0300

    Meio crud e menu

--- ListaDeAfazeres/Program.cs
+++ ListaDeAfazeres/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 using System.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -17,25 +18,89 @@ class Program
             .Build();
 
         connectionString = configuration.GetConnectionString("dados");
+        var opcao = menu();
+        while (opcao != "5")
+        {
+            if (opcao == "1")
+            {
+                adicionarTarefa();
 
+            }
+            if (opcao == "2")
+            {
+                editarTarefa();
+            }
+
+            if (opcao == "3")
+            {
+                verTarefa();
+            }
+            if (opcao == "4")
+            {
+                excluirTarefa();
+            }
+
+            menu();
+        }
+    }
+
+    static string menu()
+    {
+        var opcao = "";
+        Console.WriteLine("Você deseja: ");
+        Console.WriteLine("1 - Adicionar tarefa");
+        Console.WriteLine("2 - Editar tarefa");
+        Console.WriteLine("1 - Visualizar tarefa");
+        Console.WriteLine("1 - Excluir tarefa");
+        Console.WriteLine("5 - Sair");
+        opcao = Console.ReadLine();
+
+        return opcao;
+    }
+
+    static void adicionarTarefa()
+    {
+
+        Console.Clear();
         Console.Write("Digite a tarefa: ");
         var tarefa = Console.ReadLine();
         Console.WriteLine("Digite a data da tarefa (yyyy-mm-dd): ");
         var data = Console.ReadLine();
+        Console.WriteLine("Digite a dificuldade da tarefa: ");
+        var dificulcade = Console.ReadLine();
 
         // Connect to MySQL database and insert data
         using (var connection = new MySqlConnection(connectionString))
         {
             connection.Open();
-            var query = "INSERT INTO cad_tarefas(tarefa, data_tarefa) VALUES (@tarefa, @data)";
+            var query = "INSERT INTO cad_tarefas(tarefas, data_tarefas, dificuldade) VALUES (@tarefa, @data, @dificuldade)";
             using (var command = new MySqlCommand(query, connection))
             {
                 command.Parameters.AddWithValue("@tarefa", tarefa);
-                command.Parameters.AddWithValue("@data", DateTime.Parse(data));
+                command.Parameters.AddWithValue("@data", data);
+                command.Parameters.AddWithValue("@difculdade", dificulcade);
                 command.ExecuteNonQuery();
             }
         }
-
+        Console.Clear();
         Console.WriteLine("Tarefa inserida com sucesso.");
     }
-}
+
+    static void editarTarefa()
+    {
+
+    }
+
+    static void verTarefa()
+    {
+      
+    }
+
+    static void excluirTarefa()
+    {
+
+    }
+
+
+} 
+        
